c32531d50dc68166f6e2ca367aafd7e49f1f38d7
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-04-15 16:01:55 +0900

    make a class about nurse
    make a class about nurse

---
+++ raspberryPiCode/nurse.cpp
@@ -0,0 +1,1 @@
+#include "nurse.h"
---
+++ raspberryPiCode/nurse.h
@@ -0,0 +1,16 @@
+#ifndef SH_NURSE
+#define SH_NURSE
+#include "common.h"
+
+using namespace han;
+
+class Nurse{
+	protected:
+
+
+	public:
+
+
+};
+
+#endif /* SH_NURSE */
