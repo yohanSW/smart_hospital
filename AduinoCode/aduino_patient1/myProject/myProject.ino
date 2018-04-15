7e3b0aa29bc6250efee8ff2ef417f193005b37e5
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-04-15 14:00:53 +0900

    aduinoCode starter
    make aduino's main

---
+++ AduinoCode/aduino_patient1/myProject/myProject.ino
@@ -0,0 +1,139 @@
+
+/*  Pulse Sensor Amped 1.5    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
+
+----------------------  Notes ----------------------  ----------------------
+This code:
+1) Blinks an LED to User's Live Heartbeat   PIN 13
+2) Fades an LED to User's Live HeartBeat    PIN 5
+3) Determines BPM
+4) Prints All of the Above to Serial
+
+Read Me:
+https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md
+ ----------------------       ----------------------  ----------------------
+*/
+#include "HX711.h"
+#include <OneWire.h>
+
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER  2
+
+HX711 scale(A1, A2); // DOUT, SCK
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+int DS18S20_Pin = 2;  //방수온도센서
+OneWire ds(DS18S20_Pin); 
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// SET THE SERIAL OUTPUT TYPE TO YOUR NEEDS
+// PROCESSING_VISUALIZER works with Pulse Sensor Processing Visualizer
+//      https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
+// SERIAL_PLOTTER outputs sensor data for viewing with the Arduino Serial Plotter
+//      run the Serial Plotter at 115200 baud: Tools/Serial Plotter or Command+L
+static int outputType = SERIAL_PLOTTER;
+
+
+void setup(){
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  pinMode(A3,INPUT);
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
+   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+  //   analogReference(EXTERNAL);
+  
+  scale.set_scale(2280.f); //// this value is obtained by calibrating the scale with known weights
+  scale.tare();
+}
+
+
+//  Where the Magic Happens
+void loop(){
+  int value = analogRead(A3);
+  int limit = digitalRead(3);
+
+  float temperature = getTemp();
+  Serial.print("!");
+  
+  serialOutput() ;
+
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        fadeRate = 255;         // Makes the LED Fade Effect Happen
+                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+        QS = false;                      // reset the Quantified Self flag for next time
+  }
+
+  ledFadeToBeat();                      // Makes the LED Fade Effect Happen
+
+  //Serial.print("one reading:\t");
+  Serial.print("@");
+  Serial.print(scale.get_units(), 1);
+  Serial.print("#");
+  Serial.print(value);
+  Serial.print("$");
+  Serial.print(limit);
+  Serial.print("%");
+  Serial.println(temperature);
+
+  scale.power_down(); // put the ADC in sleep mode
+  delay(20);                             //  take a break
+  scale.power_up();
+}
+
+
+
+
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
+
+  float getTemp(){                                   //온도 측정 후 반환하는 함수
+ byte data[12];
+ byte addr[8];
+ if ( !ds.search(addr)) {
+   ds.reset_search();
+   return -1000;
+ }
+ if ( OneWire::crc8( addr, 7) != addr[7]) {
+   Serial.println("CRC is not valid!");
+   return -1000;
+ }
+ if ( addr[0] != 0x10 && addr[0] != 0x28) {
+   Serial.print("Device is not recognized");
+   return -1000;
+ }
+ ds.reset();
+ ds.select(addr);
+ ds.write(0x44,1);                                   
+ byte present = ds.reset();
+ ds.select(addr);  
+ ds.write(0xBE); 
+ 
+ for (int i = 0; i < 9; i++) { 
+  data[i] = ds.read();                                                          
+ }
+ 
+ ds.reset_search(); 
+ byte MSB = data[1];
+ byte LSB = data[0];
+ float tempRead = ((MSB << 8) | LSB); 
+ float TemperatureSum = tempRead / 16; 
+ return TemperatureSum;                                                                    
+}
