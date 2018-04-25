748acbf29d0c7c63bd493d7c20ceb3f51636441b
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-04-25 14:43:57 +0900

    Temperature Sensor DS18B20
    Temperature Sensor DS18B20

---
+++ AduinoCode/aduino_patient2/fever/Temperature_Sensor_DS18B20.ino
@@ -0,0 +1,48 @@
+/*-----( Import needed libraries )-----*/
+#include <OneWire.h>
+#include <DallasTemperature.h>
+ 
+/*-----( Declare Constants )-----*/
+#define ONE_WIRE_BUS 2 /*-(Connect to Pin 2 )-*/
+ 
+/*-----( Declare objects )-----*/
+/* Set up a oneWire instance to communicate with any OneWire device*/
+OneWire ourWire(ONE_WIRE_BUS);
+ 
+/* Tell Dallas Temperature Library to use oneWire Library */
+DallasTemperature sensors(&ourWire);
+ 
+/*-----( Declare Variables )-----*/
+ 
+ 
+void setup() /*----( SETUP: RUNS ONCE )----*/
+{
+/*-(start serial port to see results )-*/
+delay(1000);
+Serial.begin(9600);
+Serial.println("YourDuino.com: Electronic Brick Test Program");
+Serial.println("Temperature Sensor DS18B20");
+delay(1000);
+ 
+/*-( Start up the DallasTemperature library )-*/
+sensors.begin();
+}/*--(end setup )---*/
+ 
+ 
+void loop() /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+Serial.println();
+Serial.print("Requesting temperature...");
+sensors.requestTemperatures(); // Send the command to get temperatures
+Serial.println("DONE");
+ 
+Serial.print("Device 1 (index 0) = ");
+Serial.print(sensors.getTempCByIndex(0));
+Serial.println(" Degrees C");
+Serial.print("Device 1 (index 0) = ");
+Serial.print(sensors.getTempFByIndex(0));
+Serial.println(" Degrees F");
+ 
+}/* --(end main loop )-- */
+ 
+/* ( THE END ) */
