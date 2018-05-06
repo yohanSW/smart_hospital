2feefdbd2d23bc10cf43067967acdb9e0dd05509
cchom <cchom@users.noreply.github.com>
2018-05-06 15:56:58 +0900

    common header

--- raspberryPiCode/common.h
+++ raspberryPiCode/common.h
@@ -1,8 +1,17 @@
 #ifndef SH_COMMON
 #define SH_COMMON
 
-#include <iostream>
-#include <string>
-#include <pthread>
+#include <iostream.h>
+#include <pthread.h>
+#include <stdio.h> //for printf
+#include <stdint.h> //uint8_t definitions
+#include <stdlib.h> //for exit(int);
+#include <string.h> //for errno
+#include <errno.h> //error output
+#include <unistd.h>
+#include <fcntl.h>
+//wiring Pi
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
 #endif /* SH_COMMON */
