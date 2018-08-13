66d23380359d9f3528e65a3ee4310ef27861a4aa
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-08-13 14:02:43 +0900

    plus this
    plus this

--- raspberryPiCode/ICU.cpp
+++ raspberryPiCode/ICU.cpp
@@ -144,23 +144,23 @@ namespace han
 #define BAD		3
 #define BUTTON	5	*/
 	void judge_danger(){
-		if(patient1.get_temp() > DANGEROUS_TEMP)
-			patient1.set_danger(2);
-		else if(patient1.get_heart_rate() > DANGEROUS_BEAT)
-			patient1.set_danger(1);
+		if(this.patient1.get_temp() > DANGEROUS_TEMP)
+			this.patient1.set_danger(2);
+		else if(this.patient1.get_heart_rate() > DANGEROUS_BEAT)
+			this.patient1.set_danger(1);
 		else
-			patient1.set_danger(0);
+			this.patient1.set_danger(0);
 		
-		if(patient2.get_infusion_solution() < DANGEROUS_INFUSION_WEIGHT)
-			patient2.set_danger(2);
-		else if(patient2.get_heart_rate() > DANGEROUS_BEAT)
-			patient2.set_danger(1);
+		if(this.patient2.get_infusion_solution() < DANGEROUS_INFUSION_WEIGHT)
+			this.patient2.set_danger(2);
+		else if(this.patient2.get_heart_rate() > DANGEROUS_BEAT)
+			this.patient2.set_danger(1);
 		/*else if(patient2.get_wake_up() == 1)
 			patient2.set_danger(3);*/
 		else
-			patient2.set_danger(0);
+			this.patient2.set_danger(0);
 
-		cout << "fever_patient danger is --"<<patient1.get_danger() <<"-- vegeta_patient danger is --"<<patient2.get_danger() <<"--"<<endl;
+		cout << "fever_patient danger is --"<<this.patient1.get_danger() <<"-- vegeta_patient danger is --"<<this.patient2.get_danger() <<"--"<<endl;
 
 	}
 }
