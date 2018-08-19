7e24b095d00e9d9a5289e8338948dc8420fbee76
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-08-19 15:38:21 +0900

    set indentation
    set indentation

--- raspberryPiCode/ICU.cpp
+++ raspberryPiCode/ICU.cpp
@@ -90,69 +90,60 @@ void ICU::upload_data(){ //mysql
 		}	
 	}
 
-	void ICU::print(){
-		cout << "ICU print << \t room temp : " << room_temp << "\t danger_situation : " << danger_situation << endl;
-		patient1.print();
-		patient2.print();
-		nurse.print();
-		cout << endl;
-	}
+void ICU::print(){
+	cout << "ICU print << \t room temp : " << room_temp << "\t danger_situation : " << danger_situation << endl;
+	patient1.print();
+	patient2.print();
+	nurse.print();
+	cout << endl;
+}
 	
 	
-	void ICU::situation_control(){
+void ICU::situation_control(){
 
-		int danger = get_danger_situation();
-		int room_temperature = get_room_temp();
-		int set_room_temp = 26;
+	int danger = get_danger_situation();
+	int room_temperature = get_room_temp();
+	int set_room_temp = 26;
 
-		switch(danger){
-			case 1: danger = nurse.danger_emergency(); break;
-			case 2: danger = nurse.danger_ringer(); break;
-			case 3: danger = nurse.danger_situation_off(); break;
-			case 0: break;
-		}
-
-		if(set_room_temp < room_temperature){
-			digitalWrite(FAN, HIGH);
-		}
-		else digitalWrite(FAN, LOW);
-
-	}// 민지
-
-	void ICU::get_sensor(){
-		while(true){
-			patient1.get_sensor();
-			patient2.get_sensor();
-			judge_danger();
-		}
+	switch(danger){
+		case 1: danger = nurse.danger_emergency(); break;
+		case 2: danger = nurse.danger_ringer(); break;
+		case 3: danger = nurse.danger_situation_off(); break;
+		case 0: break;
 	}
-/*#define DANGEROUS_TEMP 30.0
-#define DANGEROUS_BEAT 130
+	if(set_room_temp < room_temperature){
+		digitalWrite(FAN, HIGH);
+	}
+	else digitalWrite(FAN, LOW);
+}// 민지
 
-#define GOOD	1
-#define SOSO	2
-#define BAD		3
-#define BUTTON	5	*/
-	void ICU::judge_danger(){
-		patient1.m.lock();
-		if(patient1.get_temp() > DANGEROUS_TEMP)
-			patient1.set_danger(2);
-		else if(patient1.get_heart_rate() > DANGEROUS_BEAT)
-			patient1.set_danger(1);
-		else
-			patient1.set_danger(0);
-		patient1.m.unlock();
-		patient2.m.lock();
-		if(patient2.get_infusion_solution() < DANGEROUS_INFUSION_WEIGHT)
-			patient2.set_danger(2);
-		else if(patient2.get_heart_rate() > DANGEROUS_BEAT)
-			patient2.set_danger(1);
-		/*else if(patient2.get_wake_up() == 1)
-			patient2.set_danger(3);*/
-		else
-			patient2.set_danger(0);
-		patient2.m.unlock();
-		cout << "fever_patient danger is --"<<patient1.get_danger() <<"-- vegeta_patient danger is --"<<patient2.get_danger() <<"--"<<endl;
+void ICU::get_sensor(){
+	while(true){
+		patient1.get_sensor();
+		patient2.get_sensor();
+		judge_danger();
+	}
+}
 
+void ICU::judge_danger(){
+	patient1.m.lock();
+	if(patient1.get_temp() > DANGEROUS_TEMP)
+		patient1.set_danger(2);
+	else if(patient1.get_heart_rate() > DANGEROUS_BEAT)
+		patient1.set_danger(1);
+	else
+		patient1.set_danger(0);
+	patient1.m.unlock();
+	patient2.m.lock();
+	if(patient2.get_infusion_solution() < DANGEROUS_INFUSION_WEIGHT)
+		patient2.set_danger(2);
+	else if(patient2.get_heart_rate() > DANGEROUS_BEAT)
+		patient2.set_danger(1);
+	/*else if(patient2.get_wake_up() == 1)
+		patient2.set_danger(3);*/
+	else
+		patient2.set_danger(0);
+	patient2.m.unlock();
+	cout << "fever_patient danger is --"<<patient1.get_danger() <<"-- vegeta_patient danger is --"<<patient2.get_danger() <<"--"<<endl;
 	}
 }
